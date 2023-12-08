c17d21cd7725db1d1728418ec73435fe93696585
Eugenio <eugenio.spera@gmail.com>
2023-12-08 17:30:27 -0300

    exercicio em aula

---
+++ script.js
@@ -0,0 +1,33 @@
+let passeio = prompt("Digite o tipo de veiculo:"); 
+let estadoVeiculo = prompt("Digite o estado do veículo:");
+let acidente = prompt("Digite se o carro ja bateu:");
+let totalPasseio, naoPasseio
+let totalEstadoVeiculo
+let totalAcidente
+ 
+ 
+for (let i = 1; i <= 10; i++){
+    
+if (totalPasseio = passeio){
+    alert("Passeio")
+}
+
+else { 
+    alert("naoPasseio");
+}
+ 
+if (totalEstadoVeiculo = sp){
+    alert("SP")
+}
+else { 
+    alert("RJ");
+}
+
+if (totalAcidente = sim ){
+    alert("SIM")
+}
+else { 
+    alert("NÃO");
+}   
+
+
